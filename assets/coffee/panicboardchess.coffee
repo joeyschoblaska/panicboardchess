a0b6ed3f002fe1eb18ca500b577ff7a4a0e3f354
Joey Schoblaska <joey.schoblaska@gmail.com>
2015-01-31 17:28:54 -0600

    check for a new problem every hour

--- assets/coffee/panicboardchess.coffee
+++ assets/coffee/panicboardchess.coffee
@@ -1,43 +1,58 @@
-$ ->
+window.currentPosition = ""
+
+window.setProblem = (data) ->
+  window.currentPosition = data.start
+  game = new Chess(data.start)
+  board = new ChessBoard "chessboard",
+    position: data.start
+    draggable: true
+    orientation: if game.turn() == "w" then "white" else "black"
+    onDragStart: (source, piece, position, orientation) =>
+      return !(game.game_over() ||
+               (game.turn() == "w" && piece.search(/^b/) != -1) ||
+               (game.turn() == "b" && piece.search(/^w/) != -1))
+
+    onDrop: (source, target) =>
+      oldPosition = game.fen()
+      move = game.move
+        from: source
+        to: target
+        promotion: "q"
+
+      if move != null && move.san == data.moves[0]
+        # legal and correct move
+        data.moves.shift()
+
+        if data.moves.length == 0
+          # puzzle is over
+          setTimeout (->
+            board.clear()
+          ), 500
+        else
+          # make next move for opponent
+          response = data.moves.shift()
+
+          setTimeout (->
+            game.move(response)
+            board.position(game.fen())
+          ), 250
+
+      else
+        # illegal or incorrect move
+        game.load(oldPosition)
+        return "snapback"
+
+window.requestPosition = ->
   $.ajax
     url: "/problem.json"
     success: (data) ->
-      game = new Chess(data.start)
-      board = new ChessBoard "chessboard",
-        position: data.start
-        draggable: true
-        orientation: if game.turn() == "w" then "white" else "black"
-        onDragStart: (source, piece, position, orientation) =>
-          return !(game.game_over() ||
-                   (game.turn() == "w" && piece.search(/^b/) != -1) ||
-                   (game.turn() == "b" && piece.search(/^w/) != -1))
+      setProblem(data) unless data.start == window.currentPosition
 
-        onDrop: (source, target) =>
-          oldPosition = game.fen()
-          move = game.move
-            from: source
-            to: target
-            promotion: "q"
+$ ->
+  timeout = ->
+    requestPosition()
+    setTimeout (->
+      timeout()
+    ), 3600000
 
-          if move != null && move.san == data.moves[0]
-            # legal and correct move
-            data.moves.shift()
-
-            if data.moves.length == 0
-              # puzzle is over
-              setTimeout (->
-                board.clear()
-              ), 500
-            else
-              # make next move for opponent
-              response = data.moves.shift()
-
-              setTimeout (->
-                game.move(response)
-                board.position(game.fen())
-              ), 250
-
-          else
-            # illegal or incorrect move
-            game.load(oldPosition)
-            return "snapback"
+  timeout()
--- public/javascripts/panicboardchess.js
+++ public/javascripts/panicboardchess.js
@@ -1,50 +1,70 @@
 (function() {
-  $(function() {
+  window.currentPosition = "";
+
+  window.setProblem = function(data) {
+    var board, game;
+    window.currentPosition = data.start;
+    game = new Chess(data.start);
+    return board = new ChessBoard("chessboard", {
+      position: data.start,
+      draggable: true,
+      orientation: game.turn() === "w" ? "white" : "black",
+      onDragStart: (function(_this) {
+        return function(source, piece, position, orientation) {
+          return !(game.game_over() || (game.turn() === "w" && piece.search(/^b/) !== -1) || (game.turn() === "b" && piece.search(/^w/) !== -1));
+        };
+      })(this),
+      onDrop: (function(_this) {
+        return function(source, target) {
+          var move, oldPosition, response;
+          oldPosition = game.fen();
+          move = game.move({
+            from: source,
+            to: target,
+            promotion: "q"
+          });
+          if (move !== null && move.san === data.moves[0]) {
+            data.moves.shift();
+            if (data.moves.length === 0) {
+              return setTimeout((function() {
+                return board.clear();
+              }), 500);
+            } else {
+              response = data.moves.shift();
+              return setTimeout((function() {
+                game.move(response);
+                return board.position(game.fen());
+              }), 250);
+            }
+          } else {
+            game.load(oldPosition);
+            return "snapback";
+          }
+        };
+      })(this)
+    });
+  };
+
+  window.requestPosition = function() {
     return $.ajax({
       url: "/problem.json",
       success: function(data) {
-        var board, game;
-        game = new Chess(data.start);
-        return board = new ChessBoard("chessboard", {
-          position: data.start,
-          draggable: true,
-          orientation: game.turn() === "w" ? "white" : "black",
-          onDragStart: (function(_this) {
-            return function(source, piece, position, orientation) {
-              return !(game.game_over() || (game.turn() === "w" && piece.search(/^b/) !== -1) || (game.turn() === "b" && piece.search(/^w/) !== -1));
-            };
-          })(this),
-          onDrop: (function(_this) {
-            return function(source, target) {
-              var move, oldPosition, response;
-              oldPosition = game.fen();
-              move = game.move({
-                from: source,
-                to: target,
-                promotion: "q"
-              });
-              if (move !== null && move.san === data.moves[0]) {
-                data.moves.shift();
-                if (data.moves.length === 0) {
-                  return setTimeout((function() {
-                    return board.clear();
-                  }), 500);
-                } else {
-                  response = data.moves.shift();
-                  return setTimeout((function() {
-                    game.move(response);
-                    return board.position(game.fen());
-                  }), 250);
-                }
-              } else {
-                game.load(oldPosition);
-                return "snapback";
-              }
-            };
-          })(this)
-        });
+        if (data.start !== window.currentPosition) {
+          return setProblem(data);
+        }
       }
     });
+  };
+
+  $(function() {
+    var timeout;
+    timeout = function() {
+      requestPosition();
+      return setTimeout((function() {
+        return timeout();
+      }), 3600000);
+    };
+    return timeout();
   });
 
 }).call(this);
