2f5eea50c7b19e93bf5fce13358fb56db8cc016e
Joey Schoblaska <joey.schoblaska@gmail.com>
2015-01-31 16:34:53 -0600

    only allow legal moves

--- assets/coffee/panicboardchess.coffee
+++ assets/coffee/panicboardchess.coffee
@@ -2,6 +2,23 @@ $ ->
   $.ajax
     url: "/problem.json"
     success: (data) ->
-      new ChessBoard "chessboard",
+      game = new Chess(data.start)
+      board = new ChessBoard "chessboard",
         position: data.start
         draggable: true
+        onDragStart: (source, piece, position, orientation) =>
+          return !(game.game_over() ||
+                   (game.turn() == "w" && piece.search(/^b/) != -1) ||
+                   (game.turn() == "b" && piece.search(/^w/) != -1))
+
+        onDrop: (source, target) =>
+          move = game.move
+            from: source
+            to: target
+            promotion: "q"
+
+          # illegal move
+          return "snapback" if (move == null)
+
+        onSnapEnd: =>
+          board.position(game.fen)
--- public/javascripts/panicboardchess.js
+++ public/javascripts/panicboardchess.js
@@ -3,9 +3,34 @@
     return $.ajax({
       url: "/problem.json",
       success: function(data) {
-        return new ChessBoard("chessboard", {
+        var board, game;
+        game = new Chess(data.start);
+        return board = new ChessBoard("chessboard", {
           position: data.start,
-          draggable: true
+          draggable: true,
+          onDragStart: (function(_this) {
+            return function(source, piece, position, orientation) {
+              return !(game.game_over() || (game.turn() === "w" && piece.search(/^b/) !== -1) || (game.turn() === "b" && piece.search(/^w/) !== -1));
+            };
+          })(this),
+          onDrop: (function(_this) {
+            return function(source, target) {
+              var move;
+              move = game.move({
+                from: source,
+                to: target,
+                promotion: "q"
+              });
+              if (move === null) {
+                return "snapback";
+              }
+            };
+          })(this),
+          onSnapEnd: (function(_this) {
+            return function() {
+              return board.position(game.fen);
+            };
+          })(this)
         });
       }
     });
